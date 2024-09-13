b29c31df4335349831dfb4aa119b94f4988b78b0
Franz96521 <franz96521@hotmail.com>
2024-09-12 19:54:56 -0600

    book embeding update

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ app = create_app()
 with app.app_context():
     books = Book.query.all()
     for book in books:
+        if book.embedding is not None:
+            continue
         book.generate_embedding()
         book.save()
     print('Embeddings generated and saved successfully!')
